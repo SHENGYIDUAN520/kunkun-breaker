45909d5784dd396cb170b26cef6de9b10c6c602b
SHENGYIDUAN520 <13708435621@163.COM>
2024-12-07 20:35:35 +0800

    second

--- src/game.py
+++ src/game.py
@@ -7,6 +7,7 @@ from level import *
 from block import *
 import random
 
+
 class Game(object):
     def __init__(self, surface):
         pygame.mixer.init()
@@ -15,6 +16,7 @@ class Game(object):
         self.surface = surface
         self.game_over = False
         self.restart_button = pygame.Rect(0, 0, 150, 50)  
+
     def Load(self, lv):
         self.level = Level(lv)
         self.isGameOver = False
@@ -23,25 +25,27 @@ class Game(object):
         self.loadOneBall(self.player.GetRect().x, self.player.GetRect().y - SPRITE_SIZE_H - 5, 1, -1)
         self.loadBlockImages()
 
-    
+
     def loadPlayer(self):
         self.player = Player(
             PLAYER_RES, 
             (GAME_SIZE[0] - PLAYER_SIZE_W)/2, GAME_SIZE[1] - PLAYER_SIZE_H, 
             SPRITE_SIZE_W, GAME_SIZE[0] - PLAYER_SIZE_W - SPRITE_SIZE_W)
-    
-    
+
 
     def loadBlockImages(self):
         self.blocks = []
         for block in self.level.GetBlocks():
             sp = Block(block[2], block[0], block[1], (0, 0))
             self.blocks.append(sp)
+
+
     def loadOneBall(self, x, y, dirX, dirY):
         # 生成随机的水平方向，-1 或 1
         dirX = random.choice([-1, 1])
         ball = Ball(BALL_RES, x, y, dirX, -1)
         self.balls.append(ball)
+
 
     def draw(self):
         if self.isGameOver:
@@ -49,8 +53,11 @@ class Game(object):
             self.surface.blit(img, img.get_rect())
             # 绘制绿色的重新开始按钮
             pygame.draw.rect(self.surface, (0, 255, 0), self.restart_button)  
-            font = pygame.font.Font(None, 36)
-            text = font.render("Restart", True, (255, 255, 255))  # 按钮上的文字
+            # 使用系统默认字体
+            # 使用支持中文的字体，如 SimHei
+            font = pygame.font.SysFont("SimHei", 20)
+            text = font.render("你打球像蔡徐坤", True, (255, 255, 255))  # 按钮上的文字
+
             text_rect = text.get_rect(center=self.restart_button.center)
             self.surface.blit(text, text_rect)  # 绘制按钮上的文字
             return 
@@ -61,6 +68,10 @@ class Game(object):
 
     def update(self):
         if self.isGameOver:
+            for event in pygame.event.get():
+                if event.type == MOUSEBUTTONDOWN:
+                    if self.restart_button.collidepoint(event.pos):
+                        self.Load(1)
             return
         self.player.update()
         [ball.update() for ball in self.balls]
